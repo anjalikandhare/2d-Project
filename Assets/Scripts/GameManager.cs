45c62e26bf4e2babbf5d09f9b373e2a447446f73
Addy <35691560+tambeaditya54@users.noreply.github.com>
2019-10-23 17:37:56 +0530

    Dialogue Manager Fixed

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -5,36 +5,47 @@ using TMPro;
 
 public class GameManager : MonoBehaviour {
     public TMP_Text _name;
+    string sentence;
     public TMP_Text Conversation;
     private Queue<string> sentences;
     public Spawner spawner;
     public Player player;
 
+    bool trig = true;
+    bool cinea = false;
+
     void Start() {
         sentences = new Queue<string>();
     }
     void Update() {
+        Debug.Log(sentences.Count);
+
+        if (spawner.cine == true && trig == true) {
+            
+            cinea = true;
+            FindObjectOfType<DialogueTrigger>().TriggerDialouge();
+            trig = false;
+
+        }
 
         if (spawner.cine == true) {
+            if (Input.GetKeyDown(KeyCode.Z)) {
+                DisplayNextSentence();
+            }
+        } else {
+            player.cinematics = false;
+            player.runSpeed = player.runSpeedBak;
+            FindObjectOfType<Canvas>().enabled = false;
+            cinea = false;
+        }
 
+        if (cinea) {
             player.cinematics = true;
             player.runSpeed = 0;
             Vector3 Temp = player.transform.localScale;
             Temp.x = -4;
             player.transform.localScale = Temp;
-            FindObjectOfType<DialogueTrigger>().TriggerDialouge();
             FindObjectOfType<Canvas>().enabled = true;
-
-            if (Input.GetKeyDown(KeyCode.Z)) {
-                DisplayNextSentence();
-            }
-
-        } else {
-
-            player.cinematics = false;
-            player.runSpeed = player.runSpeedBak;
-            FindObjectOfType<Canvas>().enabled = false;
-
         }
     }
 
@@ -47,16 +58,18 @@ public class GameManager : MonoBehaviour {
         {
             sentences.Enqueue(sentence);
         }
+
     }
 
     public void DisplayNextSentence() {
-        string sentence = sentences.Dequeue();
-        StopAllCoroutines();
-        StartCoroutine(TypeSentence(sentence));
         if (sentences.Count == 0) {
             EndDialogue();
             return;
         }
+        sentence = sentences.Dequeue();
+        StopAllCoroutines();
+        StartCoroutine(TypeSentence(sentence));
+        
     }
 
     IEnumerator TypeSentence (string sentence) {
